82b82e554102b2248d544c1928eddfc63f33ced3
Agnese Marchesini <agnese.mar@icloud.com>
2026-05-05 15:47:07 +0200

    feat: establish connection to MongoDB using Mongoose

---
+++ app.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri)
+    .then(() => console.log("Connected to the database!"))
+    .catch((err) => console.log("Connection error: ", err));
