ace2ea90dc5d8e979a5eb1ec11a57c2f81ae5634
Agnese Marchesini <agnese.mar@icloud.com>
2026-05-05 22:43:52 +0200

    chore: setup cors

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const express = require("express")
-const app = express()
-const routeAuth = require("./routes/routeAuth")
+const cors = require("cors");
+const app = express();
+const routeAuth = require("./routes/routeAuth");
 
 require("dotenv").config();
 const uri = process.env.MONGODB_URI;
@@ -10,7 +11,7 @@ mongoose.connect(uri)
     .then(() => console.log("Connected to the database!"))
     .catch((err) => console.log("Connection error: ", err));
 
-
+app.use(cors());
 app.use(express.json());
 app.use("/auth", routeAuth);
 app.listen(3000);
