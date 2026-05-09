2f007ee508268a98c8f8b44688604f50166e4fad
Alessandro Bignami <alebig04@gmail.com>
2026-05-09 22:03:57 +0200

    feat: added mission schema and created routeMission file

---
+++ models/missione.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+
+const STATO_MISSIONE = ['DaIniziare', 'InCorso', 'Completata', 'Annullata', 'InPausa'];
+
+const missionSchema = new mongoose.Schema({
+    percorso: {
+        type: String,
+        required: true
+    },
+    punti: {
+        type: Number,
+        min: 0,
+        required: true
+    },
+    bonusGamification: {
+        type: Number,
+        min: 0
+    },
+    risparmioCO2: {
+        type: mongoose.Decimal128,
+        min: 0
+    },
+    stato: {
+        type: String,
+        enum: STATO_MISSIONE
+    }
+})
+
+module.exports = mongoose.model("Missione", missionSchema)
