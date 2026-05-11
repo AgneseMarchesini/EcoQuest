131a8ea8c641f91d7b51a99bd0425856bdd12ad4
MichUni <michele.zanetti-4@studenti.unitn.it>
2026-05-11 17:00:35 +0200

    fix: added .get on the route

--- routes/routePOI.js
+++ routes/routePOI.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const poi = require('../models/POI');
+const path = require('path');
+
+router.get("/view_map", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/view_map.html"));
+});
 
 router.get('/puntiVicini', async (req, res)=> {
     try{
-        // se il frontend non passa le coordinate usiamo quelle del centro di Trento
         const latitudine = parseFloat(req.query.latitudine) || 46.066423;
         const longitudine = parseFloat(req.query.longitudine) || 11.125760;
 
-        // raggio default = 3km
         const raggioInMetri = parseInt(req.query.raggio) || 3000;
 
         const nearbyPOIs = await poi.find({
